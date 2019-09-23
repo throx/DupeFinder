e173cb2298360ae670e438e59b4693c4bde4aa8d
John Wiltshire <jw@chase.net.au>
2019-09-23 21:58:17 +1000

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace DupeFinder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.src1 = new System.Windows.Forms.TextBox();
+            this.src2 = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.resultsView = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.hash1MToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hashToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.candidateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.resultsView)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // src1
+            // 
+            this.src1.Location = new System.Drawing.Point(145, 12);
+            this.src1.Name = "src1";
+            this.src1.Size = new System.Drawing.Size(340, 26);
+            this.src1.TabIndex = 0;
+            this.src1.Text = "f:\\recover";
+            // 
+            // src2
+            // 
+            this.src2.Location = new System.Drawing.Point(145, 54);
+            this.src2.Name = "src2";
+            this.src2.Size = new System.Drawing.Size(340, 26);
+            this.src2.TabIndex = 1;
+            this.src2.Text = "g:\\root\\video";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(520, 13);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(210, 67);
+            this.start.TabIndex = 2;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // resultsView
+            // 
+            this.resultsView.AllowUserToAddRows = false;
+            this.resultsView.AllowUserToDeleteRows = false;
+            this.resultsView.AllowUserToResizeRows = false;
+            this.resultsView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultsView.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            this.resultsView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultsView.ContextMenuStrip = this.contextMenuStrip1;
+            this.resultsView.Location = new System.Drawing.Point(12, 120);
+            this.resultsView.Name = "resultsView";
+            this.resultsView.ReadOnly = true;
+            this.resultsView.RowTemplate.Height = 28;
+            this.resultsView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.resultsView.Size = new System.Drawing.Size(1208, 1099);
+            this.resultsView.TabIndex = 3;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hash1MToolStripMenuItem,
+            this.hashToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.toolStripSeparator1,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(156, 130);
+            // 
+            // hash1MToolStripMenuItem
+            // 
+            this.hash1MToolStripMenuItem.Name = "hash1MToolStripMenuItem";
+            this.hash1MToolStripMenuItem.Size = new System.Drawing.Size(155, 30);
+            this.hash1MToolStripMenuItem.Text = "Hash 1M";
+            this.hash1MToolStripMenuItem.Click += new System.EventHandler(this.hash1MToolStripMenuItem_Click);
+            // 
+            // hashToolStripMenuItem
+            // 
+            this.hashToolStripMenuItem.Name = "hashToolStripMenuItem";
+            this.hashToolStripMenuItem.Size = new System.Drawing.Size(155, 30);
+            this.hashToolStripMenuItem.Text = "Hash";
+            this.hashToolStripMenuItem.Click += new System.EventHandler(this.hashToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(155, 30);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(13, 1225);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(1207, 26);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 4;
+            this.progressBar.Value = 50;
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(155, 30);
+            this.toolStripMenuItem1.Text = "Launch";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(152, 6);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(753, 13);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(201, 67);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // candidateButton
+            // 
+            this.candidateButton.Location = new System.Drawing.Point(973, 13);
+            this.candidateButton.Name = "candidateButton";
+            this.candidateButton.Size = new System.Drawing.Size(195, 67);
+            this.candidateButton.TabIndex = 6;
+            this.candidateButton.Text = "Candidates";
+            this.candidateButton.UseVisualStyleBackColor = true;
+            this.candidateButton.Click += new System.EventHandler(this.candidateButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1232, 1263);
+            this.Controls.Add(this.candidateButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.resultsView);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.src2);
+            this.Controls.Add(this.src1);
+            this.Name = "MainForm";
+            this.Text = "Dupe Finder";
+            ((System.ComponentModel.ISupportInitialize)(this.resultsView)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox src1;
+        private System.Windows.Forms.TextBox src2;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.DataGridView resultsView;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hash1MToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hashToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button candidateButton;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,385 @@
+﻿using Microsoft.Collections.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DupeFinder
+{
+    public partial class MainForm : Form
+    {
+        DataTable dataTable = new DataTable("Dupe Data");
+        int count;
+        int num_done;
+        string saveFile;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            var nameCol = dataTable.Columns.Add("Name", typeof(string));
+            var sizeCol = dataTable.Columns.Add("Size", typeof(long));
+            var h1kCol = dataTable.Columns.Add("Hash1k", typeof(string));
+            var h1mCol = dataTable.Columns.Add("Hash1M", typeof(string));
+            var hCol = dataTable.Columns.Add("Hash", typeof(string));
+            dataTable.PrimaryKey = new DataColumn[] { nameCol };
+
+            saveFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "DupeFinder.xml");
+            if (File.Exists(saveFile) && MessageBox.Show("Load saved session?", "Dupe Finder", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using (var str = File.OpenRead(saveFile))
+                {
+                    dataTable.ReadXml(str);
+                }
+            }
+
+            resultsView.DataSource = dataTable;
+            resultsView.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            resultsView.Columns[1].DefaultCellStyle.Format = "N0";
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            dataTable.Clear();
+            foreach (var f in Directory.EnumerateFiles(src1.Text, "*", SearchOption.AllDirectories))
+            {
+                var fi = new FileInfo(f);
+                dataTable.Rows.Add(new object[] { f, fi.Length });
+            }
+            foreach (var f in Directory.EnumerateFiles(src2.Text, "*", SearchOption.AllDirectories))
+            {
+                var fi = new FileInfo(f);
+                dataTable.Rows.Add(new object[] { f, fi.Length });
+            }
+
+            num_done = 0;
+
+            timer1.Tick += SizeDups;
+            timer1.Start();
+        }
+
+        private void SizeDups(object sender, EventArgs e)
+        {
+            timer1.Tick -= SizeDups;
+            timer1.Stop();
+
+            var sizes = new Dictionary<long, List<string>>();
+            foreach (var row in dataTable.AsEnumerable())
+            {
+                string f = row.Field<string>(0);
+                long s = row.Field<long>(1);
+
+                if (sizes.TryGetValue(s, out List<string> fl))
+                {
+                    fl.Add(f);
+                }
+                else
+                {
+                    sizes.Add(s, new List<string>() { f });
+                }
+            }
+            foreach (var item in sizes)
+            {
+                if (item.Value.Count == 1)
+                {
+                    dataTable.Rows.Find(item.Value.First()).Delete();
+                }
+            }
+
+            count = dataTable.Rows.Count;
+
+            timer1.Tick += DoHash1k;
+            timer1.Start();
+        }
+
+        private void DoHash1k(object sender, EventArgs e)
+        {
+            timer1.Tick -= DoHash1k;
+            timer1.Stop();
+            DateTime t = DateTime.Now;
+
+            progressBar.Value = 100 * num_done / count;
+
+            foreach (var row in dataTable.AsEnumerable().
+                Where(val => val.Field<string>("Hash1k") == null))
+            {
+                using (var br = new BinaryReader(File.OpenRead(row.Field<string>("Name"))))
+                {
+                    var bytes = br.ReadBytes(10240);
+                    using (SHA256 sha = SHA256.Create())
+                    {
+                        var hash = sha.ComputeHash(bytes);
+                        row.SetField<string>("Hash1k", BitConverter.ToString(hash));
+                        num_done++;
+                    }
+                }
+
+                if ((DateTime.Now -t).TotalMilliseconds > 500)
+                {
+                    timer1.Tick += DoHash1k;
+                    timer1.Start();
+                    return;
+                }
+            }
+
+            progressBar.Value = 0;
+            timer1.Tick += HashDups;
+            timer1.Start();
+        }
+
+        private Dictionary<string, List<string>> GetDupeList()
+        {
+            var hashes = new Dictionary<string, List<string>>();
+            foreach (var row in dataTable.AsEnumerable())
+            {
+                string f = row.Field<string>(0);
+                string h = row.Field<long?>(1).ToString() + row.Field<string>(2);
+                if (row.Field<string>(3) != null)
+                {
+                    h += row.Field<string>(3);
+                    num_done++;
+                }
+                if (row.Field<string>(4) != null)
+                {
+                    h += row.Field<string>(4);
+                    num_done++;
+                }
+
+                if (hashes.TryGetValue(h, out List<string> fl))
+                {
+                    fl.Add(f);
+                }
+                else
+                {
+                    hashes.Add(h, new List<string>() { f });
+                }
+            }
+
+            var files = new Dictionary<string, List<string>>();
+            foreach (var item in hashes)
+            {
+                foreach (var f in item.Value)
+                {
+                    files.Add(f, item.Value.Where(x => x != f).ToList());
+                }
+            }
+            return files;
+        }
+
+        private void HashDups()
+        {
+            var hashes = new Dictionary<string, List<string>>();
+            foreach (var row in dataTable.AsEnumerable())
+            {
+                string f = row.Field<string>(0);
+                string h = row.Field<long?>(1).ToString() + row.Field<string>(2);
+                if (row.Field<string>(3) != null)
+                {
+                    h += row.Field<string>(3);
+                    num_done++;
+                }
+                if (row.Field<string>(4) != null)
+                {
+                    h += row.Field<string>(4);
+                    num_done++;
+                }
+
+                if (hashes.TryGetValue(h, out List<string> fl))
+                {
+                    fl.Add(f);
+                }
+                else
+                {
+                    hashes.Add(h, new List<string>() { f });
+                }
+            }
+            foreach (var item in hashes)
+            {
+                if (item.Value.Count == 1)
+                {
+                    dataTable.Rows.Find(item.Value.First()).Delete();
+                }
+            }
+        }
+
+        private void HashDups(object sender, EventArgs e)
+        {
+            timer1.Tick -= HashDups;
+            timer1.Stop();
+
+            HashDups();
+        }
+
+        private void DoHash1m(string path)
+        {
+            using (var br = new BinaryReader(File.OpenRead(path)))
+            {
+                var bytes = br.ReadBytes(1024 * 1024);
+                using (SHA256 sha = SHA256.Create())
+                {
+                    var hash = sha.ComputeHash(bytes);
+                    var row = dataTable.Rows.Find(path);
+                    row.SetField<string>("Hash1m", BitConverter.ToString(hash));
+                }
+            }
+        }
+
+        private void DoHash(string path)
+        {
+            //using (SHA256 sha = SHA256.Create())
+            //{
+            //    var hash = sha.ComputeHash(File.OpenRead(path));
+            //    var row = dataTable.Rows.Find(path);
+            //    row.SetField<string>("Hash", BitConverter.ToString(hash));
+            //}
+            var len = (new FileInfo(path)).Length;
+            using (var fs = File.OpenRead(path))
+            {
+                byte[] buffer = new byte[16 * 1024];
+                using (SHA256 sha = SHA256.Create())
+                {
+                    for (int segment = 0; segment < 16; segment++)
+                    {
+                        long pos = len * segment / 16L;
+                        fs.Seek(pos, SeekOrigin.Begin);
+                        fs.Read(buffer, segment * 1024, 1024);
+                    }
+                    var hash = sha.ComputeHash(buffer);
+                    var row = dataTable.Rows.Find(path);
+                    row.SetField<string>("Hash", BitConverter.ToString(hash));
+                }
+            }
+        }
+
+        private void hash1MToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var hashes = GetDupeList();
+
+            foreach (var s in resultsView.SelectedRows)
+            {
+                var row = ((s as DataGridViewRow).DataBoundItem as DataRowView).Row;
+                string f = row.Field<string>(0);
+                DoHash1m(f);
+                
+                if (hashes.TryGetValue(f, out var others))
+                {
+                    foreach (var x in others)
+                    {
+                        DoHash1m(x);
+                    }
+                }
+            }
+            HashDups();
+        }
+
+        private void hashToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var hashes = GetDupeList();
+
+            progressBar.Value = 0;
+            int c = resultsView.SelectedRows.Count;
+            int n = 0;
+
+            var toHash = new HashSet<string>();
+
+            foreach (var s in resultsView.SelectedRows)
+            {
+                var row = ((s as DataGridViewRow).DataBoundItem as DataRowView).Row;
+                string f = row.Field<string>(0);
+                toHash.Add(f);
+            }
+
+            foreach (var f in toHash)
+            {
+                DoHash(f);
+
+                if (hashes.TryGetValue(f, out var others))
+                {
+                    foreach (var x in others)
+                    {
+                        if (!toHash.Contains(x))
+                        DoHash(x);
+                    }
+                }
+
+                n++;
+                progressBar.Value = 100 * n / c;
+            }
+            progressBar.Value = 0;
+            timer1.Tick += HashDups;
+            timer1.Start();
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            progressBar.Value = 0;
+            int c = resultsView.SelectedRows.Count;
+            int n = 0;
+
+            foreach (var s in resultsView.SelectedRows)
+            {
+                var row = ((s as DataGridViewRow).DataBoundItem as DataRowView).Row;
+                Microsoft.VisualBasic.FileIO.FileSystem.DeleteFile(row.Field<string>(0),
+                    Microsoft.VisualBasic.FileIO.UIOption.OnlyErrorDialogs,
+                    Microsoft.VisualBasic.FileIO.RecycleOption.SendToRecycleBin);
+                row.Delete();
+                n++;
+                progressBar.Value = 100 * n / c;
+            }
+            progressBar.Value = 0;
+            timer1.Tick += HashDups;
+            timer1.Start();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var row = (resultsView.CurrentRow.DataBoundItem as DataRowView).Row;
+            using (Process proc = new Process())
+            {
+                proc.StartInfo.FileName = row.Field<string>(0);
+                proc.StartInfo.UseShellExecute = true;
+                proc.Start();
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            using (var str = File.Create(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "DupeFinder.xml")))
+            {
+                dataTable.WriteXml(str);
+            }
+        }
+
+        private void candidateButton_Click(object sender, EventArgs e)
+        {
+            resultsView.ClearSelection();
+
+            var files = GetDupeList();
+
+            foreach (var s in resultsView.Rows)
+            {
+                var dgvr = s as DataGridViewRow;
+                var row = (dgvr.DataBoundItem as DataRowView).Row;
+                var f = row.Field<string>(0);
+                if (f.StartsWith(src1.Text))
+                {
+                    if (files.TryGetValue(f, out var others))
+                    {
+                        if (others.Count == 1 &&
+                            others.First().StartsWith(src2.Text))
+                        {
+                            dgvr.Selected = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
